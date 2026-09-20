70a58c922037cd75af82513f375edf31dbac5a9b
staskyryk2004 <staskyryk@gmail.com>
2026-09-21 00:54:20 +0300

    Add files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './main.css';
 import './head.css';
-//import File from './File.js';
+import File from './File.js';
 //import Program from './Program.js';
-import Student from './Student.js';
+//import Student from './Student.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Student />
+    <File />
   </React.StrictMode>
 );
