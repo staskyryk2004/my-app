c2ab033de7adba468e7920e624f008549a7be084
staskyryk2004 <staskyryk@gmail.com>
2026-09-21 01:32:54 +0300

    Add files

--- src/Student.js
+++ src/Student.js
@@ -3,10 +3,10 @@ function Student(){
         <>
         <h2>Освітня діяльність</h2>
         <form class="form">
-        <p>Освітній ступінь</p>
+        <b><p>Освітній ступінь</p></b>
         <input type="radio" name="education" value="Bachelor"/> Бакалавр<br></br>
         <input type="radio" name="education" value="Magister"/> Магістр<br></br>
-        <p>Термін навчання</p>
+        <b><p>Термін навчання</p></b>
         <input type="radio" name="education" value="Full"/> Повний<br></br>
         <input type="radio" name="education" value="Short"/> Скорочений<br></br>
         </form>
