7826b94012ef52f0402b0b20e7703ea312af0636
staskyryk2004 <staskyryk@gmail.com>
2026-09-22 20:10:54 +0300

    Add files

--- src/Money.js
+++ src/Money.js
@@ -3,34 +3,25 @@ class Money extends React.Component {
     render(){
         return(
         <>
-        <h2 className="main-title">Внесення реквізитів про підвищення кваліфікації</h2>
-        <form className="form-container">
-        <fieldset className="form-box">
-        <legend className="box-title">Семестр вивчення ОК</legend>
-        <div className="checkbox-row">
-        <label className="checkbox-label">
-        <input type="checkbox" name="education" value="semester"/>1 сем. <br></br>
-        </label>
-        <input type="checkbox" name="education" value="semester"/>2 сем. <br></br>
-        <label className="checkbox-label">
-        <input type="checkbox" name="education" value="semester"/>3 сем.<br></br>
-        </label>
-        <input type="checkbox" name="education" value="semester"/>4 сем. <br></br>
-        <label className="checkbox-label">
-        <input type="checkbox" name="education" value="semester"/>5 сем. <br></br>
-        </label>
-        <input type="checkbox" name="education" value="semester"/>6 сем. <br></br>
-        <label className="checkbox-label">
-        <input type="checkbox" name="education" value="semester"/>7 сем. <br></br>
-        </label>
+        <h2>Семестр вивчення</h2>
+        <form>
+        <fieldset className="box">
+        <legend>Семестр вивчення ОК</legend>
+        <div className="semester-row">
+        <label><input type="checkbox" name="education" value="semester"/> 1 сем.</label>
+        <label><input type="checkbox" name="education" value="semester"/> 2 сем.</label>
+        <label><input type="checkbox" name="education" value="semester"/> 3 сем.</label>
+        <label><input type="checkbox" name="education" value="semester"/> 4 сем.</label>
+        <label><input type="checkbox" name="education" value="semester"/> 5 сем.</label>
+        <label><input type="checkbox" name="education" value="semester"/> 6 сем.</label> 
+        <label><input type="checkbox" name="education" value="semester"/> 7 сем.</label>
         </div>
         </fieldset>
-        <fieldset className="form-box">
-         <legend className="box-title">Приєднання файлу</legend> 
-         <div className="file-section">  
-        <input type="checkbox" name="language" value="file"/>Прикріпити файл документа до ОК <br></br>
-        <div className="file-upload">
-        <label htmlFor="file-input">Виберіть файл ОК (тільки PDF):</label>
+        <fieldset className="box">
+         <legend>Приєднання файлу</legend> 
+         <div className="file-box"> 
+        <div className="file-line">
+        <label>Виберіть файл ОК (тільки PDF):</label>
             <input type="file" name="file-input" accept=".pdf" /> 
             </div>
           </div> 
