1a514c6e5e0c6696625e367a2e1f49519f2fe97b
staskyryk2004 <staskyryk@gmail.com>
2026-09-22 20:18:15 +0300

    Add files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import './main.css';
 import './head.css';
 import './form.css';
-import './money.css';
+import './education.css';
 //import File from './File.js';
 //import Program from './Program.js';
 //import Student from './Student.js';
